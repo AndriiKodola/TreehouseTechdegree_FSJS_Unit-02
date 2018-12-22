687c14278c50ac8a83aee8e349722070f8ebbe02
AndriiKodola <ankodola@gmail.com>
2018-12-22 15:02:30 +0100

    Update script.js
    Mainly to prevent HTML collection from syncronization with DOM structure, and search function work properly between different pages.
    Also minor refubrishment

--- js/script.js
+++ js/script.js
@@ -10,13 +10,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
   const listContainer = document.getElementsByClassName('page')[0];
-        studentListItems = document.getElementsByClassName('student-item');
-        navPanel = document.createElement('nav');
+  const studentCollection = document.getElementsByClassName('student-item');
+  const navPanel = document.createElement('nav');
+  const studentListItems = [];
+
+  //Student array creation for preventig collection syhc with HTML structure
+  for ( let i = 0; i<studentCollection.length; i++) {
+    studentListItems.push(studentCollection[i]);
+  }
 
   //Paginates passed studet list by creating of an array (of pages) of arrays (of students per page)
   const paginateList = (list) => {
     const paginatedList = [];
-          perPageList = [];
+    let perPageList = [];
 
     for (let i = 0; i < list.length; i++) {
       perPageList.push(list[i]);
@@ -35,19 +41,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
   //creates list items for passed page number and replaces existing list with the one created
   const showPage = (pageNum) => {
-    const studentList = document.getElementsByClassName('student-list');
-          perPageList = [];
+    const studentList = document.getElementsByClassName('student-list')[0];
+    const currentPageList = paginatedList[pageNum-1];
+    let perPageList = [];
 
-    for (let i = 0; i < paginatedList[pageNum-1].length; i++) {
-       perPageList += `
-        <li class="student-item cf">
-          ${paginatedList[pageNum-1][i].innerHTML}
-        </li>
+    for (let i = 0; i < currentPageList.length; i++) {
+        let perPageStudentItem = currentPageList[i].innerHTML;
+        perPageList += `
+          <li class="student-item cf">
+          ${perPageStudentItem}
+          </li>
         `;
     }
-    console.log(studentList[0].innerHTML);
-    studentList[0].innerHTML = perPageList;
-    console.log(studentList[0].innerHTML);
+    studentList.innerHTML = perPageList;
   };
 
   //shows first page for a start
@@ -86,51 +92,51 @@ document.addEventListener('DOMContentLoaded', () => {
 /***--------------Exceed expectations content----------------------***/
 
 //Creating and styling according to design.css search nodes
-const pageHeader = document.getElementsByClassName('page-header')[0];
+  const pageHeader = document.getElementsByClassName('page-header')[0];
 
-const searchDiv = document.createElement('div');
-      searchField = document.createElement('input');
-      searchButton = document.createElement('button');
-      searchDiv.classList.add('student-search');
-      searchField.type = 'search';
-      searchButton.type = 'button';
-      searchButton.textContent = 'Search';
+  const searchDiv = document.createElement('div');
+  const searchField = document.createElement('input');
+  const searchButton = document.createElement('button');
+  searchDiv.classList.add('student-search');
+  searchField.type = 'search';
+  searchButton.type = 'button';
+  searchButton.textContent = 'Search';
 
-      searchDiv.appendChild(searchField);
-      searchDiv.appendChild(searchButton);
-      pageHeader.appendChild(searchDiv);
+  searchDiv.appendChild(searchField);
+  searchDiv.appendChild(searchButton);
+  pageHeader.appendChild(searchDiv);
 
 
-      searchButton.addEventListener('click', () => {
-        const foundList = [];
-              searchName = searchField.value;
-              searchField.value = '';//empty the search field
+  searchButton.addEventListener('click', () => {
+    const foundList = [];
+    const searchName = searchField.value;
+    searchField.value = '';//empty the search field
 
-        //creating a list of matches
-        for (let i = 0; i < studentListItems.length; i++ ) {
-          const currentStudent = studentListItems[i];
-                studentName = currentStudent.getElementsByTagName('h3')[0].textContent;
+    //creating a list of matches
+    for (let i = 0; i < studentListItems.length; i++ ) {
+      const currentStudent = studentListItems[i];
+      const studentName = currentStudent.getElementsByTagName('h3')[0].textContent;
 
-          if (studentName === searchName) {
-            foundList.push(currentStudent);
-          }
-        }
+      if (studentName === searchName) {
+        foundList.push(currentStudent);
+      }
+    }
 
-        //Displays message if no matches found or match list if there were any
-        if (!foundList.length) {
-          const searchMessage = document.createElement('span');
-                searchMessage.textContent = 'No matches found.';
+    //Displays message if no matches found or match list if there were any
+    if (!foundList.length) {
+      const noMatchMessage = document.createElement('span');
+      noMatchMessage.textContent = 'No matches found.';
 
-                searchDiv.prepend(searchMessage);
-                setTimeout(searchDiv.removeChild(searchDiv.childNodes[0]), 3000);
-        } else {
-          //paginating matches list
-          paginatedList = paginateList(foundList);
+            searchDiv.prepend(noMatchMessage);
+            setTimeout(function(){searchDiv.removeChild(searchDiv.childNodes[0])}, 3000);
+    } else {
+      //paginating matches list
+      paginatedList = paginateList(foundList);
 
-          //editing DOM structure to display search results
-          showPage(1);
-          navPanel.innerHTML = '';//deleting previous nav bar
-          appendPageLinks(paginatedList);
-        }
-      });
+      //editing DOM structure to display search results
+      showPage(1);
+      navPanel.innerHTML = '';//deleting previous nav bar
+      appendPageLinks(paginatedList);
+    }
+  });
 });
